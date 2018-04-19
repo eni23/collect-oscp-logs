908155b17c8e6ca6f3b51a7c3473bb4b69926fc6
Cyrillvw <cyrill.vonwattenwyl@adfinis-sygroup.ch>
2018-04-19 13:52:04 +0200

    add files

---
+++ collect-oscp-logs.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import subprocess
+
+log_location = "/var/log/oscp-logs"
+
+def main():
+
+  try:
+    oc_output = subprocess.check_output([
+      "oc",
+      "get",
+      "pods",
+      "--all-namespaces"
+    ])
+  except subprocess.CalledProcessError:
+    print("failed to get pods")
+    sys.exit(0)
+
+  pods = oc_output.split("\n")[1:]
+
+  for pod in pods:
+    if pod:
+      pod = " ".join(pod.split())
+      row = pod.split(" ")
+      namespace = row[0]
+      pod = row[1]
+
+      cmd = [
+        "oc",
+        "logs",
+        pod,
+        "--namespace={0}".format(namespace)
+      ]
+      filename = time.strftime(
+        '{0}/%Y-%m-%d-%H-%M-%S_{1}_{2}'.format(
+          log_location,namespace, pod
+      ))
+
+      print("processing pod {0} in namespace {1}".format(
+        pod,
+        namespace
+      ))
+
+      try:
+        log_out = subprocess.check_output(cmd)
+        with open(filename, 'w') as logfile:
+          logfile.write(log_out)
+      except subprocess.CalledProcessError:
+        print("failed to get logs")
+
+  sys.exit(0)
+
+
+if __name__ == '__main__':
+  main()
